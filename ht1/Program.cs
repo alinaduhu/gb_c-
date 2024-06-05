363dadf2b9f19dd6c2eb37804d7d599e32051bb7
Alina Duhu <metalyugina12@icloud.com>
2024-06-05 23:36:37 +0300

    added 1 task

---
+++ ht1/Program.cs
@@ -0,0 +1,26 @@
+﻿// // Задача 1: Задайте значения M и N. Напишите программу,
+// //  которая выведет все натуральные числа в промежутке 
+// //  от M до N. Использовать рекурсию, не использовать
+// //   циклы.
+
+
+void PrintNaturalNumbers(int firstNum, int secondNum)
+{
+    if(firstNum > secondNum ) return;
+    
+    Console.WriteLine($"{firstNum} ");
+    
+    PrintNaturalNumbers(firstNum + 1, secondNum);
+
+}
+
+Console.WriteLine("Введите натуральное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+PrintNaturalNumbers(m, n);
+
