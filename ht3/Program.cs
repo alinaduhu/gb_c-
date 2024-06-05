4792ee423b64311032d921e44ad5171e7e9f9327
Alina Duhu <metalyugina12@icloud.com>
2024-06-05 23:45:17 +0300

    added task3

---
+++ ht3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его 
+// элементы, начиная с конца. Использовать рекурсию, не 
+// использовать циклы.
+
+
+void PrintArrayReverse(int[] array, int i)
+{
+   if (i <= 0) return;
+   Console.Write(array[i]);
+    PrintArrayReverse( array , i - 1);
+}
+
+
+int[] arr = {3, 5, 1, 8, 0, 7};
+PrintArrayReverse(arr, arr.Length - 1);
